3a9acc9e3baa19b7ed5d4395551c8eeb3526cc2b
HerrBergenholtz <zbdv8800@gmail.com>
2023-09-15 12:44:09 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            float elin = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            float alma = float.Parse(Console.ReadLine());
+
+            float elinvinst = elin - alma;
+            Console.WriteLine("Elin hoppade " + elinvinst + " meter längre än Alma");
+
+            Console.ReadKey();
+        }
+    }
+}
